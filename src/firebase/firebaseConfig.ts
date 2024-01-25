6928a1970dde9d12ba63df3a1e0d8011862c77ee
andrescuco <andrescuellarco@gmail.com>
2024-01-25 17:03:00 -0500

    Cleanup

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,14 +1,14 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import { getAuth } from "firebase/auth";
 
 export const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAkCsNls7aY4YQdDm4Zw2p0lsK_tlMikao",
+  authDomain: "infobae-challenge.firebaseapp.com",
+  projectId: "infobae-challenge",
+  storageBucket: "infobae-challenge.appspot.com",
+  messagingSenderId: "832425144479",
+  appId: "1:832425144479:web:96d076fe68fdf55eeeb2aa",
+  measurementId: "G-5FHGPVYYJL"
 };
 
 // Initialize Firebase
